29e3cddb557f9d91154032e8ae90e93c6607634c
juan1sanchez <32147273+juan1sanchez@users.noreply.github.com>
2020-02-04 17:08:12 -0800

    Update
    Added an error checking loop.  Updated userCharacter to use raw_input instead of input.  Changed some print statements to allow proper display format.

--- lastToZero.py
+++ lastToZero.py
@@ -6,33 +6,43 @@ def clear():
 		os.system('cls')
 	else:
 		os.system('clear')
-
+	return
 
 def howTo():
-	print("Countdown is a game where we start off with 10 \
-		\nand subtract 1, 2, or 3.  The objective to force\
-		\nthe other player to land on 0.  Have fun!\n \n ")
+	clear()
 
-userCharacter = "a"
+	print("Countdown is a game where we start off with 10 \
+		\nand subtract 1, 2, or 3.  The objective is to force\
+		\nthe other player to land on 0.  Have fun!\n \n ")
+		
+	return
+	
+userCharacter = 'y'
 
 print("Welcome to Countdown!\n")
 
-while ( userCharacter == "a"):
+while ( userCharacter == 'y'):
 
 	gameNumber = 10
 	
 	while ( gameNumber > 0):
 	
 		howTo()
-		print("Current Number: ", gameNumber)
+		userNumber = 0
+		print "Current Number: ", gameNumber
 		
-		userNumber = int(input("Enter a 1,2,3: "))
+		#Error checking while loop, makes sure input is either 1,2, or 3
+		while ( userNumber == 0):
+		
+			userNumber = int(input("Enter a 1,2,3: "))
+			if userNumber <= 0 or userNumber > 3:
+				print("Invalid number, please try again")
+				userNumber = 0
+				
 		gameNumber -= userNumber
 		
 		if (gameNumber <= 0):
 			print("You Lost")
-			userCharacter = input("To play again, enter 'a': ")
-		
-		clear()
+			userCharacter = raw_input("To play again, enter 'y': ")
 	
 
